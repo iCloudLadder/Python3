a98405fd253e49ff885ee51f0a781e2d48a36712
weishiyue <w_shiyue@163.com>
2017-10-24 16:15:12 +0800

    Python3
    Study Python3

---
+++ StudyPython3/HelloWorld.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 15:27:14 2017
+
+@author: cooperlink
+"""
+
+
+
+############# 输出 ####################
+
+print("Hello World!")
+print('Hello World!!')
+
+# 函数也可以接受多个字符串，用逗号“,”隔开，就可以连成一串输出
+print('Hello', 'World', '!')
+
+print(200)
+print("100 + 200 =", 100 + 200)
+
+print('1024 * 768 =', 1024 *768)
+
+
+############# 输入 ####################
+
+aNumber = input()
+print('input number is', aNumber)
+
+name = input('Please input your name:')
+print('Your name is', name)
+
